4bc3396295cb1c9e8724439903e44fb3b7da2aad
Gabriel <oviciado65@gmail.com>
2026-08-28 21:14:53 -0300

    agora ele PULA
    UAU

--- Assets/Aula 05/Scripts/CharacterMove.cs
+++ Assets/Aula 05/Scripts/CharacterMove.cs
@@ -5,11 +5,26 @@ public class CharacterMove : MonoBehaviour
 {
     private float speed = 5f;
     public Rigidbody rb;
+    private Vector3 direction;
+
+    void Update()
+    {
+        direction = Vector3.zero;
+
+        HandleMovement();
+        HandleCrouchAndRun();
+        HandleJump();
+
+    }
 
     void FixedUpdate()
     {
-        Vector3 direction = Vector3.zero;
+        direction = Vector3.ClampMagnitude(direction, 1f);
+        rb.linearVelocity = new Vector3(direction.x * speed, rb.linearVelocity.y, direction.z * speed);
+    }
 
+    void HandleMovement()
+    {
         if (Keyboard.current[Key.W].isPressed)
         {
             direction += transform.forward;
@@ -25,29 +40,35 @@ public class CharacterMove : MonoBehaviour
         {
             direction += transform.right * -1;
         }
+    }
 
+    void HandleCrouchAndRun()
+    {
         if (Keyboard.current[Key.LeftCtrl].isPressed)
         {
             speed = 2.5f;
+            rb.mass = 1.25f;
         }
         else
         {
             if (Keyboard.current[Key.LeftShift].isPressed)
             {
                 speed = 7.5f;
+                rb.mass = 0.75f;
             }
             else
             {
                 speed = 5f;
+                rb.mass = 1f;
             }
         }
+    }
 
-        if (Keyboard.current[Key.Space].isPressed)
+    void HandleJump()
+    {
+       if (Keyboard.current[Key.Space].wasPressedThisFrame)
         {
-            rb.AddForce(new Vector3(direction.x * 100f, rb.linearVelocity.y, direction.z * 100f), ForceMode.Impulse);
-        }
-
-        direction = Vector3.ClampMagnitude(direction, 1f);
-        rb.linearVelocity = new Vector3(direction.x * speed, rb.linearVelocity.y, direction.z * speed);
+            rb.AddForce(new Vector3(0f, 10f, 0f),ForceMode.Impulse);
+        } 
     }
 }
